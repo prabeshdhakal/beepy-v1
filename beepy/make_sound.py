7a17f1041ec5cbbeaaac27d189a11f4d751de70a
felipesilvadv <lfsilva1@uc.cl>
2021-02-25 13:58:38 -0300

    TypeError for invalid input sound

--- beepy/make_sound.py
+++ beepy/make_sound.py
@@ -25,12 +25,15 @@ def beep(sound=1):
                    'success',
                    'wilhelm'
     ]
-    if isinstance(sound, int):
+    if isinstance(sound, int) or isinstance(sound, float):
+        sound = int(sound)
         if sound not in range(1, len(sound_names) + 1):
             sound = 4
     elif isinstance(sound, str):
         assert sound in sound_names, f'Name "{sound}" does not exists'
         sound = sound_names.index(sound) + 1
+    else:
+        raise TypeError('sound must be a int or string')
 
     wave_obj_file = wave_dict.get(sound, 'ping.wav')
     WAVE_PATH = os.path.join(AUDIO_DIR, wave_obj_file)
